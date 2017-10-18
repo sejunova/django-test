e3f676e6e6d8c4436e85b9172ec7ed64ad4af294
Sejun Im <sejun425@gmail.com>
2017-10-18 12:12:03 +0900

    post_create 성공

--- wed_test/post/views.py
+++ wed_test/post/views.py
@@ -23,15 +23,17 @@ def post_detail(request, post_pk):
 def post_create(request):
     if request.method == "POST":
         post_form = PostForm(request.POST, request.FILES)
-        title = post_form.cleaned_data['title']
-        photo = post_form.cleaned_data['photo']
-        content = post_form.cleaned_data['content']
-
-        Post.objects.create(title=title, photo=photo, content=content)
-        return redirect('post_list')
+        if post_form.is_valid():
+            title = post_form.cleaned_data['title']
+            photo = post_form.cleaned_data['photo']
+            content = post_form.cleaned_data['content']
+            Post.objects.create(title=title, photo=photo, content=content)
+            return redirect('post_list')
     else:
         post_form = PostForm()
     context = {
         'post_form': post_form,
     }
     return render(request, 'post/post_create.html', context)
+
+def post_delete()
